36dab46143e50ff12ae14ee00320eeb3a6d80929
Virginia Balseiro <virginiabalseiro@gmail.com>
2018-04-29 16:18:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("#search").click(function(){
+    var searchTerm = $("#searchTerm").val();
+    var wikiLink = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=" + searchTerm + "&origin=*";
+   $.ajax(wikiLink, {
+      dataType: "json",
+      type: "GET",
+      success: function(data) {
+        $("#output").html("");
+        for (var i=0; i<data[1].length; i++){
+          $("#output").prepend("<li>" + data[2][i] + "</li></li></br>");
+         $("#output").prepend("<li><li> <a href=" +(data[3][i])+ ">"  + data[1][i] + "</a></li>");
+        };
+       },
+     error: function (errorMessage){
+       alert("There was a problem. Try again soon!");
+     }
+   })
+  })
+
+  $("#social-button").on("click", function() {
+    $("#social").toggle();
+    $("#ellipsis").toggle();
+  })
+
+
+
+})//end of document ready
